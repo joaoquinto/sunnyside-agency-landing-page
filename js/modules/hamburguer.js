ed69d449c3af333ff43b2b1cd532edf42c4d0766
João Victor Oliveira Pereira <joaovictoroliveirapereira6522@gmail.com>
2022-05-31 16:06:48 +0000

    Modulo de acesssibilidade para menuHamburguer

--- js/modules/hamburguer.js
+++ js/modules/hamburguer.js
@@ -1,8 +1,26 @@
-const containerLinks = document.querySelector("#container-links")
-const hamburguer = document.querySelector("#hamburguer")
+const containerLinks = document.querySelector("#container-links");
+const hamburguer = document.querySelector("#hamburguer");
 
 export default function handleHamburguer() {
-  containerLinks.classList.toggle("visible")
+  containerLinks.classList.toggle("visible");
+  if (containerLinks.classList.contains("visible")) {
+    containerLinks.ariaExpanded = "true";
+  } else {
+    containerLinks.ariaExpanded = "false";
+  }
 }
 
-hamburguer.addEventListener('click', handleHamburguer)
+export function acessibilidade() {
+  const mediaMatch = matchMedia("(max-width: 799px)").matches;
+  if (mediaMatch) {
+    containerLinks.ariaOrientation = "vertical";
+    hamburguer.ariaHidden = "false";
+  } else {
+    containerLinks.ariaOrientation = "horizontal";
+    hamburguer.ariaHidden = "true";
+  }
+}
+
+window.onresize = acessibilidade;
+
+hamburguer.addEventListener("click", handleHamburguer);
