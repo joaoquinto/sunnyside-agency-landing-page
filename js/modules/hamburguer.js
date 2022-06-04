a8f3adc731154db2d45cf608f4f2adc41ba0b35a
João Victor Oliveira Pereira <joaovictoroliveirapereira6522@gmail.com>
2022-06-04 13:13:45 +0000

    issue #4

--- js/modules/hamburguer.js
+++ js/modules/hamburguer.js
@@ -1,30 +1,30 @@
 const containerLinks = document.querySelector("#container-links");
 const hamburguer = document.querySelector("#hamburguer");
-const titleWeAre = document.querySelector('.title-we-are')
+const titleWeAre = document.querySelector(".title-we-are");
+const mediaMatch = matchMedia("(max-width: 799px)").matches;
 
 export default function handleHamburguer() {
   containerLinks.classList.toggle("visible");
   if (containerLinks.classList.contains("visible")) {
     containerLinks.ariaExpanded = "true";
-    titleWeAre.classList.add('invisible')
+    titleWeAre.classList.add("invisible");
   } else {
     containerLinks.ariaExpanded = "false";
-    titleWeAre.classList.remove('invisible')
+    titleWeAre.classList.remove("invisible");
   }
 }
 
+hamburguer.addEventListener("click", handleHamburguer);
+
 export function acessibilidade() {
-  const mediaMatch = matchMedia("(max-width: 799px)").matches;
   if (mediaMatch) {
     containerLinks.ariaOrientation = "vertical";
     hamburguer.ariaHidden = "false";
   } else {
     containerLinks.ariaOrientation = "horizontal";
     hamburguer.ariaHidden = "true";
-    titleWeAre.classList.remove('invisible')
+    titleWeAre.classList.remove("invisible");
   }
 }
 
 window.onresize = acessibilidade;
-
-hamburguer.addEventListener("click", handleHamburguer);
